2a18c7b325f5657d91a2cf427a6eabfcb2446c9e
AjMing <62439174+AjMing@users.noreply.github.com>
2021-02-05 17:51:03 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "linkedlist.h"
 
 
 int main(int argc, char **argv){
-Lnode head;
+Lnode *head=NULL;
+
 int i,id,score;
 argv++;
 while(*argv){
    id=atoi(*argv);
-   if(id!=0 && *(argv+1) {
-   score=atoi(*(argv+1));
-   printf("Inserting %d:%d\n",id,score);
-  head=insert(id,score);
-  argv+=2;
-}
+   if(id!=0 && *(argv+1))
+      {
+      score=atoi(*(argv+1));
+    printf("Inserting %d:%d\n",id,score);
+    head=insert(head,id,score);  // แทร
+    argv+=2;
+   }
 }
    printlist(head);
    Find_avg(head);
