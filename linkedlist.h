c18ebd8e576af2994b21c86e674c50246e535483
AjMing <62439174+AjMing@users.noreply.github.com>
2021-02-05 17:50:01 +0700

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,12 @@
+//
+//  linkedlist.h
+//  LinkedList_simple
+//
+//  Created by Mingmanas Sivaraksa on 5/2/2564 BE.
+//
+
+#ifndef linkedlist_h
+#define linkedlist_h
 struct node
 {
     int id,score;
@@ -5,6 +14,29 @@ struct node
 };
 
 typedef struct node Lnode;
-Lnode insert(id,score);
-void printlist(head);
-void Find_avg(head);
+
+Lnode* insert(Lnode*head , int id,int score){
+  Lnode *t;
+  if(head==NULL){
+     head=(Lnode*) malloc(sizeof(Lnode));
+     head->id=id;
+     head->score=score;
+     head->next=NULL;
+  }
+  else {
+      t=head;
+      /* ปรับปรุงตรงนี้*/
+      t->next =(Lnode*) malloc(sizeof(Lnode));
+       /* ปรับปรุงตรงนี้*/
+  }
+  
+  return head;
+}
+
+
+void printlist(Lnode*); // print id, score ให้ครบ
+void Find_avg(Lnode*); // คำนวน average  score
+
+
+#endif /* linkedlist_h */
+
